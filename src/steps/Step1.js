f22ef30345dfed4b7dcc46e1044aaaac272cd5b6
Nayemul <nayemulhasan2016@gmail.com>
2021-07-06 02:53:11 +0600

    custom style form with material-ui

--- src/steps/Step1.js
+++ src/steps/Step1.js
@@ -1,20 +1,33 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
-
+import MainContainer from '../components/formComponents/MainContainer';
+import Form from '../components/formComponents/Form'
+import Input from '../components/formComponents/Input';
+import PrimaryButton from '../components/formComponents/PrimaryButton';
 const Step1 = () => {
     const {register, handleSubmit, errors} = useForm();
     const onSubmit =(data)=>{
         console.log(data);
     }
     return (
-        <div>
+        <MainContainer>
             <h1>step 1</h1>
-            <form onSubmit={handleSubmit(onSubmit)} >
-                <input type="text" name="firstName" />
-                <input type="text" name="lastName" />
-                <button>Next</button>
-            </form>
-        </div>
+            <Form onSubmit={handleSubmit(onSubmit)} >
+                <Input 
+                    {...register}
+                    type="text"
+                    label="First Name" 
+                    name="firstName" 
+                />
+                <Input 
+                    {...register}
+                    type="text"
+                    label="Last Name" 
+                    name="lastName" 
+                />
+                <PrimaryButton>Next</PrimaryButton>
+            </Form>
+        </MainContainer>
     )
 }
 
