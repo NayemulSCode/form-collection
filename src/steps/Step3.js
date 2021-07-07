b78a249e5bb1b804988bc9906b5933a0acde2795
Nayemul <nayemulhasan2016@gmail.com>
2021-07-07 18:35:16 +0600

    step3 structure done

--- src/steps/Step3.js
+++ src/steps/Step3.js
@@ -1,10 +1,30 @@
+import { Typography } from '@material-ui/core';
 import React from 'react'
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
+import MainContainer from '../components/formComponents/MainContainer';
+import Form from '../components/formComponents/Form'
+import { useData } from '../context/DataContext'
+import PrimaryButton from '../components/formComponents/PrimaryButton';
 
 const Step3 = () => {
+    const {setValues, data} = useData();
+    const history = useHistory();
+    const {control, handleSubmit} = useForm({
+
+    })
+    const onSubmit =(data)=>{
+        setValues(data);
+        console.log(data);
+    }
     return (
-        <div>
-            <h1>Step 3</h1>
-        </div>
+        <MainContainer>
+            <Typography component="h2" variant="h5">Step 3</Typography>
+            <Form onSubmit = {handleSubmit(onSubmit)}>
+                <input type="file" name="files" control={control} />
+                <PrimaryButton>Next</PrimaryButton>
+            </Form>
+        </MainContainer>
     )
 }
 
